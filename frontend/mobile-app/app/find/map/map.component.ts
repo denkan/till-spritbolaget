31d1157cbe1afbb5fcb53ca5ccf9a62f58825d68
Dennis Nyström <dennis.nystrom@gmail.com>
2017-12-05 23:08:22 +0100

    [FE:MA] Map: nicer marker

--- frontend/mobile-app/app/find/map/map.component.ts
+++ frontend/mobile-app/app/find/map/map.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { registerElement } from 'nativescript-angular/element-registry';
 import { MapView, Marker, Position, Style, Bounds } from 'nativescript-google-maps-sdk';
-import { Image } from 'ui/image/image';
+import { Image } from 'ui/image';
+import { GridLayout } from 'ui/layouts/grid-layout';
 
 import * as mapStyles from './map.styles';
-import { GeolocationService } from '../../shared/geolocation';
+import { GeolocationService, LatLng } from '../../shared';
+import { FindService } from '../find.service';
 
 // Important - must register MapView plugin in order to use in Angular templates
 registerElement('MapView', () => MapView);
+
+export const enum MARKER_TYPES { ME, STORE };
 
 
 @Component({
     moduleId: module.id,
     selector: 'TheMap',
     template: `
-    <GridLayout>
+    <GridLayout #container rows="auto,*">
         <MapView row="1" #mapView [latitude]="latitude" [longitude]="longitude"
             [zoom]="zoom" [bearing]="bearing"
             [tilt]="tilt" (mapReady)="onMapReady($event)"
@@ -31,6 +35,7 @@ registerElement('MapView', () => MapView);
 `,
 })
 export class MapComponent implements OnInit {
+    @ViewChild('container') container: ElementRef;
 
     latitude =  60;
     longitude = 15;
@@ -42,40 +47,61 @@ export class MapComponent implements OnInit {
 
     lastCamera: String;
 
+    myLocation: LatLng;
+
     constructor(
-        private geolocation: GeolocationService
+        private geolocation: GeolocationService,
+        private findService: FindService,
     ) {
     }
 
     ngOnInit() {
     }
 
-    //Map events
     onMapReady(event) {
-        console.log('Map Ready');
-
         this.mapView = event.object;
-
         this.mapView.setStyle(<Style>mapStyles.DARK);
         
         this.geolocation.currentLocation$
             .filter(pos => !!pos)
+            .map(pos => LatLng.fromObject(pos))
             .subscribe(pos => {
-                this.latitude = pos.latitude;
-                this.longitude = pos.longitude;
-                this.zoom = 12;
+                this.myLocation = pos;
+                this.setMyLocationMarker(pos);
+            });
+        
+    }
 
-                this.mapView.clear();
+    setMyLocationMarker(pos?: LatLng) {
+        pos = pos || this.myLocation;
 
-                const marker = new Marker();
-                marker.position = Position.positionFromLatLng(pos.latitude, pos.longitude);
-                // marker.title = "Du";
-                // marker.snippet = "Du";
-                // marker.userData = {index: 1};
-                this.mapView.addMarker(marker);
-            })
+        // clear old 
+        const oldMarker = this.mapView.findMarker(m => m.userData.type === MARKER_TYPES.ME);
+        if(oldMarker) this.mapView.removeMarker(oldMarker);
 
+        // add new
+        const marker = new Marker();
+        marker.position = Position.positionFromLatLng(pos.latitude, pos.longitude);
+        marker.title = "Du";
+        // marker.snippet = "Du";
+        marker.userData = { type: MARKER_TYPES.ME };
 
+        const img = new Image();
+        img.src = 'res://icon';
+        img.width = 20;
+        img.height = 20;
+        //img.stretch = 'none';
+        //marker.icon = img;
+        marker.color = '#F2C112';
+
+        this.mapView.addMarker(marker);
+        this.zoomMap();
+    }
+
+    zoomMap() {
+        this.latitude = this.myLocation.latitude;
+        this.longitude = this.myLocation.longitude;
+        this.zoom = 12;
     }
 
 }
