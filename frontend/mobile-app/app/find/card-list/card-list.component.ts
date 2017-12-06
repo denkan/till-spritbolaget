6a00b3bc5478f02ce78146843657bc0fa0633f60
Dennis Nyström <dennis.nystrom@gmail.com>
2017-12-06 10:56:41 +0100

    [FE:MA] Fixed card position on scroll

--- frontend/mobile-app/app/find/card-list/card-list.component.ts
+++ frontend/mobile-app/app/find/card-list/card-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, ViewChild, ElementRef, EventEmitter } from '@angular/core';
-import { ScrollView } from "ui/scroll-view";
+import { ScrollView, ScrollEventData } from "ui/scroll-view";
 import { Size, UIService } from "../../shared";
 
 @Component({
@@ -18,36 +18,61 @@ export class CardListComponent implements OnInit {
     scrollList: ScrollView;
 
     pageSize: Size;
-    cardStyle: { 
-        width: number, 
-        margin: number, 
-        offsetX: number 
+    cardStyle: {
+        width: number,
+        margin: number,
+        offsetX: number
     };
 
     constructor(
         private ui: UIService,
     ) { }
 
-    ngOnInit() { 
+    ngOnInit() {
+        this.scrollList = <ScrollView>this.scrollListRef.nativeElement;
         this.ui.pageSize$.subscribe(size => this.setCardStyle(size));
     }
 
-
     setCardStyle(pageSize: Size) {
-        if(!pageSize || !pageSize.width) return;
+        if (!pageSize || !pageSize.width) return;
 
         this.pageSize = pageSize;
 
         this.cardStyle = {
-            width: this.pageSize.width * 0.6, // 80%
-            margin: 10, //this.pageWidth * 0.1, // 10%
-            offsetX: this.pageSize.width * 0.2 - 10,
+            width: Math.floor(this.pageSize.width * 0.6), // 60%
+            margin: 10, //old: this.pageWidth * 0.1,
+            offsetX: Math.floor(this.pageSize.width * 0.2 - 10),
         }
         return true;
     }
 
     onSelect(item) {
-        this.selectedItemIndex = this.items.findIndex(r => r.id===item.id);
+        this.selectedItemIndex = this.items.findIndex(r => r.id === item.id);
         this.select.emit(item);
+    }
+
+    private scrollTimer: number = null;
+    onScroll(e: ScrollEventData) {
+        clearTimeout(this.scrollTimer);
+
+        this.scrollTimer = setTimeout(() => {
+
+            // find and scroll to closest card
+            const
+                cardWidth = this.cardStyle.width + (this.cardStyle.margin * 2),
+                cardCount = this.items.length,
+                scrollWidth = cardWidth * cardCount,
+                x = e.scrollX,
+                closestCardRate = ((x + cardWidth / 2) / scrollWidth) * cardCount,
+                closestCardIndex = Math.floor(closestCardRate);
+
+            this.scrollToCard(closestCardIndex);
+
+        }, 50);
+    }
+
+    scrollToCard(index: number) {
+        const cardWidth = this.cardStyle.width + (this.cardStyle.margin * 2);
+        this.scrollList.scrollToHorizontalOffset((index * cardWidth), true);
     }
 }
